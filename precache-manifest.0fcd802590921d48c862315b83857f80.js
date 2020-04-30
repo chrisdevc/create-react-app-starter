3b8fc3e7d80f197a9626279e1f1ae94bf43ea028
Chris Ciantar <chris.ciantar@riverigaming.com>
2020-04-30 14:20:16 +0200

    Updates

---
+++ precache-manifest.0fcd802590921d48c862315b83857f80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cde710fe894e711b98524c3c6948113",
+    "url": "/create-react-app-starter/index.html"
+  },
+  {
+    "revision": "c027a1bd566de764114a",
+    "url": "/create-react-app-starter/static/css/main.e32a1458.chunk.css"
+  },
+  {
+    "revision": "76c882cd35d9e49c41e2",
+    "url": "/create-react-app-starter/static/js/2.93a3b8ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app-starter/static/js/2.93a3b8ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c027a1bd566de764114a",
+    "url": "/create-react-app-starter/static/js/main.462e03e6.chunk.js"
+  },
+  {
+    "revision": "5fb9dd3c8b2065b6da42",
+    "url": "/create-react-app-starter/static/js/runtime-main.ded51ec7.js"
+  }
+]);
