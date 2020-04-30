9fcec02d5796616465c521389c298ec3eb6e57cb
Chris Ciantar <chris.ciantar@riverigaming.com>
2020-04-30 14:19:37 +0200

    feat(*): init commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './card.scss';
+
+export const Card = (props) => {
+	return <div className="card-container">
+	<img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x280`}/>
+		<h2>{props.monster.name}</h2>
+		<p>{props.monster.email}</p>
+	</div>;
+}
